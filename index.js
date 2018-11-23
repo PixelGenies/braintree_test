8a0ec0265f8dff1b95ec43d3b3da62422fd0a92d
Mohd Sadiq <mohd144512@st.jmi.ac.in>
2018-11-24 01:29:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ app.post("/checkout", function (req, res) {
   var nonceFromTheClient = req.body
   // Use payment method nonce here
   console.log('nonce : ' + nonceFromTheClient)
-  console.log('body 1: ' + request.body)
-  console.log('body 2: ' + req.body)
+  console.log('body : ' + req.body)
   gateway.transaction.sale({
     amount: "10.00",
     paymentMethodNonce: nonceFromTheClient,
